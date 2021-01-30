b878b2c4811635ba2f1ae0ff77342e5a8d859a7e
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-29 19:30:56 -0800

    Update.

--- emacs/init-local.el
+++ emacs/init-local.el
@@ -1,7 +1,6 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
-
 (desktop-save-mode 0)
 
 
@@ -14,17 +13,14 @@
 ;;
 ;; Rust settings
 ;;
-
 ;; Indentation
 ;; The Rust style guide recommends spaces rather than tabs for indentation; to follow the recommendation add this to your init.el, which forces indentation to always use spaces.
 (add-hook 'rust-mode-hook
           (lambda () (setq indent-tabs-mode nil)))
-
 ;; Rust code formatting settings
 ;; disable rustfmt... :) comment this out to turn it back on
 ;;(setq rust-format-buffer nil)
 ;; (setq rust-format-on-save t)
-
 ;; Clippy
 
 
@@ -38,6 +34,7 @@
 (global-unset-key (kbd "M-<down-mouse-1>"))
 (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
 
+
 ;;
 ;; Opacity settings
 ;;
@@ -48,30 +45,19 @@
 ;;
 ;; Font settings
 ;;
-
 (set-frame-font "Liberation Mono 10" nil t)
-
-
 
 
 ;;
 ;; Python settings
 ;;
 ;; (elpy-enable)
-
 ;; Use IPython for REPL
-
 ;; (setq python-shell-interpreter "jupyter"
 ;;       python-shell-interpreter-args "console --simple-prompt"
 ;;       python-shell-prompt-detect-failure-warning nil)
 ;; (add-to-list 'python-shell-completion-native-disabled-interpreters
 ;;              "jupyter")
-
-
-
-
-
-
 
 
 (provide 'init-local)
